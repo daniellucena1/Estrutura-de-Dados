9e670974b92b8ff0097a4a1b2d18242d64e1aadc
Rodrigo Belarmino <rodrigo.belarmino17@gmail.com>
2023-12-06 10:44:53 -0300

    pilhas

---
+++ pilha.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Stack
+{
+  int data;
+  struct Stack *next;
+} Stack;
+
+Stack criaPilha(int novo)
+{
+  Stack *topo = malloc(sizeof(Stack));
+  topo->data = novo;
+  topo->next = NULL;
+  return *topo;
+}
+
+int verificarVazio(Stack *topo)
+{
+  if (topo == NULL)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+void imprimirPilha(Stack *topo)
+{
+  Stack *topoAtual = topo;
+
+  while (topoAtual != NULL)
+  {
+    printf("%d \n", topoAtual->data);
+    topoAtual = topoAtual->next;
+  }
+}
+
+Stack *push(Stack *topo, int novo)
+{
+  Stack *novoTopo = malloc(sizeof(Stack));
+
+  if (novoTopo == NULL)
+    return topo;
+
+  novoTopo->data = novo;
+  novoTopo->next = topo;
+
+  return novoTopo;
+}
+
+Stack *pop(Stack *topo)
+{
+  Stack *novoTopo = malloc(sizeof(Stack));
+
+  if (novoTopo == NULL)
+    return topo;
+
+  novoTopo = topo->next;
+  free(topo);
+  return novoTopo;
+}
+
+int main()
+{
+  int opcao = -1;
+  Stack *pilha = NULL;
+
+  while (opcao != 0)
+  {
+    printf(">> Digite: \n 1 - Empilhar \n 2 - Desempilhar \n 3 - Imprimir Pilha \n 0 - Sair \n -> ");
+    scanf("%d", &opcao);
+    switch (opcao)
+    {
+    case 1:
+      int novo;
+      printf("Qual item deseja empilhar: ");
+      scanf("%d", &novo);
+      pilha = push(pilha, novo);
+      break;
+    case 2:
+      pilha = pop(pilha);
+      break;
+    case 3:
+      printf("Imprimindo...\n");
+      imprimirPilha(pilha);
+    case 0:
+      break;
+    }
+  }
+}
